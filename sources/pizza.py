24a1a236ff707720fc12ca56bca70678abc80d87
Arnaud Maxime <marnaud194@gmail.com>
2023-05-17 22:14:59 +0200

    My new pizza

--- sources/pizza.py
+++ sources/pizza.py
@@ -35,5 +35,9 @@ RECIPES = {
     'mediterranea': {
         'ingredients': {'mozzarella': 3, 'pepper': 4, 'onion': 3},
         'price': 11,
-    }
+    },
+    'my pizza': {
+        'ingredients': {'gorgonzola': 4, 'mozzarella': 3},
+        'price': 11,
+    },
 }
